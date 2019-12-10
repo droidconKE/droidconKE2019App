3899f7a30535f7f594d9dcd4e6e4c7d14cc2512d
johnGachihi <johngachihi3@gmail.com>
2019-12-10 19:03:04 +0300

    Refactor WifiDetailsRepo
    Use firebase ktx await() instead of suspendCancellableCoroutine.
    Also use runCatching for coroutine.
    Also move WifiDetailsModel creation logic to a Factory

--- app/src/main/java/com/android254/droidconke19/repository/WifiDetailsRepo.kt
+++ app/src/main/java/com/android254/droidconke19/repository/WifiDetailsRepo.kt
@@ -2,35 +2,23 @@ package com.android254.droidconke19.repository
 
 import com.android254.droidconke19.datastates.FirebaseResult
 import com.android254.droidconke19.models.WifiDetailsModel
-import com.android254.droidconke19.models.isPropertiesNotEmpty
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resume
+import com.android254.droidconke19.datastates.runCatching
+import com.android254.droidconke19.models.utils.WifiDetailsModelFactory
+import kotlinx.coroutines.tasks.await
 
 interface WifiDetailsRepo {
     suspend fun fetchWifiDetails(): FirebaseResult<WifiDetailsModel>
 }
 
-class WifiDetailsRepoImpl(private val firebaseRemoteConfig: FirebaseRemoteConfig): WifiDetailsRepo {
+class WifiDetailsRepoImpl(private val firebaseRemoteConfig: FirebaseRemoteConfig) : WifiDetailsRepo {
 
     override suspend fun fetchWifiDetails(): FirebaseResult<WifiDetailsModel> =
-            suspendCancellableCoroutine { continuation ->
-                firebaseRemoteConfig.fetchAndActivate().addOnCompleteListener {
+            runCatching {
+                // Fetch Firebase RemoteConfig values
+                firebaseRemoteConfig.fetchAndActivate().await()
 
-                    val wifiDetailsModel = WifiDetailsModel(
-                            firebaseRemoteConfig.getString("wifi_ssid"),
-                            firebaseRemoteConfig.getString("wifi_password")
-                    )
-
-                    if (wifiDetailsModel.isPropertiesNotEmpty) {
-                        continuation.resume(FirebaseResult.Success(wifiDetailsModel))
-                    } else {
-//                        continuation.resumeWithException(Exception(
-//                                "No FRC default values provided for wifi details"))
-                        continuation.resume(FirebaseResult.Error(
-                                "No FRC default values provided for wifi details"))
-                    }
-                }.addOnCanceledListener { continuation.cancel() }
+                // Create an instance of WifiDetailsModels from active Firebase RemoteConfig values
+                WifiDetailsModelFactory.create(firebaseRemoteConfig)
             }
-
 }
